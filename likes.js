0596af2e96254ace558f307fa2e42b3e597764f7
GuyCalloway <guyfcalloway@gmail.com>
2020-01-05 20:32:54 +0000

    Third test passing

--- likes.js
+++ likes.js
@@ -5,6 +5,8 @@ const process = {
         return "no one likes this";
       case 1:
         return names[0] + " likes this";
+      case 2:
+        return names[0] + " and " + names[1] + " like this";
     }
   }
 };
--- likes.test.js
+++ likes.test.js
@@ -4,6 +4,10 @@ test("if no names in list returns string notifying user of this", () => {
   expect(process.likes([])).toBe("no one likes this");
 });
 
-test("if one name in list returns string notifying user of who likes", () => {
+test("if one name in list returns string notifying user of likes", () => {
   expect(process.likes(["james"])).toBe("james likes this");
+});
+
+test("if two names in list returns string notifying user of likes correctly", () => {
+  expect(process.likes(["james", "andy"])).toBe("james and andy like this");
 });
